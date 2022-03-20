af859dc02d69dacd855c4613079b0274a7f3237d
MIHIR AGRAWAL <agarwalmihir08@gmail.com>
2022-03-20 11:19:01 +0530

    Database on Cloud

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://localhost:27017/todolistDB");
+mongoose.connect("mongodb+srv://mihir:mihir@cluster0.nkmim.mongodb.net/todolistDB");
 
 const itemsSchema = {
   name:String
